7e1e1028c51e556e10bc7b7971ad9395f58db39a
Alzagardh <lucaslima2401@gmail.com>
2024-09-03 19:41:46 -0300

    Add files via upload

---
+++ aula3_algoritimos.py
@@ -0,0 +1,16 @@
+# # #lista - coleções
+lista = [10,20,30,40]
+
+# print(len(lista))
+
+# for i in range(len(lista)):
+#     print(lista[i])
+    
+# i = 0
+# while i<len(lista):
+#     print(lista[i])
+#     i += 1
+    
+for i in range(len(lista)):
+    print(i)
+    print(lista[i])
